edb283444887f93753af61ad53fdcf38f4d16ec8
Pi <cpranaviyer@gmail.com>
2021-02-17 11:28:03 -0700

    adding isntall script

---
+++ install_all.sh
@@ -0,0 +1,30 @@
+#Instructions for setting up openssl and liboqs 
+#----------------------------------------------
+#Assumptions: Ubuntu 20.04
+#------------
+#Build lsquic & OQS OpenSSL library:
+#-----------------------------
+
+sudo apt install cmake gcc libtool libssl-dev make ninja-build git python3-pytest python3-pytest-xdist unzip xsltproc doxygen graphviz
+cd ~
+rm -rf openssl
+rm -rf liboqs 
+
+git clone https://github.com/open-quantum-safe/liboqs.git
+
+git clone https://github.com/prchander/openssl.git
+
+OPENSSL_DIR=$PWD/openssl
+cd ~
+
+
+cd liboqs
+mkdir build && cd build
+cmake -GNinja -DBUILD_SHARED_LIBS=ON ..
+cmake -GNinja -DCMAKE_INSTALL_PREFIX=OPENSSL_DIR/oqs ..
+ninja
+
+cd ~
+cd openssl
+./Configure no-shared linux-x86_64 -lm
+sudo make-j
