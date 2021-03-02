9c7bb71ba947ab4c6fc49859a0428f44b97d418e
Pi <pchandra@uccs.edu>
2021-03-02 08:32:02 +0000

    errors

--- CA-project/combined_logger_generic.py
+++ CA-project/combined_logger_generic.py
@@ -9,7 +9,7 @@ num_samples = int(input("Enter number of times to loop: \n"))
 openssl_dir = os.path.expanduser('~/openssl')
 
 def initCert(algorithm, bits = ''):
-	if algorithm == 'rsa':
+	if algorithm== 'rsa':
 		myCmd = f'{openssl_dir}/apps/openssl req -x509 -new -newkey rsa -keyout key_CA.key -out key_CA.pem -pkeyopt rsa_keygen_bits:{bits} -nodes -subj "/CN=oqstest CA" -days 365 -config {openssl_dir}/apps/openssl.cnf > /dev/null 2>&1'
 		os.system(myCmd)
 	else:
@@ -17,17 +17,17 @@ def initCert(algorithm, bits = ''):
 		os.system(myCmd)
 
 def genKey(algorithm, num_samples, bits = ''):
-	if algorithm == 'rsa':
-		myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithm rsa -out {openssl_dir}/CA-project/csr/key_srv.key -pkeyopt rsa_keygen_bits:{bits} > /dev/null 2>&1'
+	if algorithm== 'rsa':
+		myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithmsrsa -out {openssl_dir}/CA-project/csr/key_srv.key -pkeyopt rsa_keygen_bits:{bits} > /dev/null 2>&1'
 		for i in range (num_samples):
 			os.system(myCmd)
 	else:
-		myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithm {algorithm} -out {openssl_dir}/CA-project/csr/key_srv.key > /dev/null 2>&1'
+		myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithms{algorithm} -out {openssl_dir}/CA-project/csr/key_srv.key > /dev/null 2>&1'
 		for i in range (num_samples):
 			os.system(myCmd)
 
 def genCSR(algorithm, num_samples, bits = ''):
-	if algorithm == 'rsa':
+	if algorithm== 'rsa':
 		myCmd = f'{openssl_dir}/apps/openssl req -new -key {openssl_dir}/CA-project/csr/key_srv.key -out {openssl_dir}/CA-project/csr/key_srv.csr -nodes -pkeyopt rsa_keygen_bits:{bits} -subj \'/CN=oqstest server\' -config {openssl_dir}/apps/openssl.cnf > /dev/null 2>&1'
 		for i in range (num_samples):
 			os.system(myCmd)
@@ -46,7 +46,7 @@ def certVerify(algorithm, num_samples, bits = ''):
 	for i in range (num_samples):
 		os.system(myCmd)
 
-algorithms = [
+algorithm = [
 	'rsa','dilithium2', 'dilithium3', 'dilithium5', 'falcon512', 'falcon1024', 'dilithium2_aes', 'dilithium3_aes', 'dilithium5_aes', 'rsa3072_dilithium2','rsa3072_falcon512', 'p256_dilithium2', 'p256_falcon512','p384_dilithium3', 'p521_dilithium5', 'p521_falcon1024'] 
 algorithms_in_english = [
 	'RSA', 'Dilithium 2', 'Dilithium 3', 'Dilithium 5', 'Falcon 512', 'Falcon 1024', 'Dilithium 2 + AES', 'Dilithium 3 + AES', 'Dilithium 5 + AES', 'RSA 3072 + Dilithium 2','RSA 3072 + Falcon 512', 'P256 + Dilithium 2', 'P256 + Falcon 512','P384 + Dilithium 3', 'P521 + Dilithium 5', 'P521 + Falcon 1024'] 
@@ -64,10 +64,10 @@ def header():
 def run(file):
 	rsa_bits_array = [2048, 3072, 4096];
 	
-	for algorithm in algorithms:
+	for i in range(len(algorithm)):
 		algorithm_in_english = algorithms_in_english[i]
 
-		if algorithm == 'rsa':
+		if algorithm== 'rsa':
 			for bits in rsa_bits_array:
 				print(f'Starting {algorithm} {bits}...')
 				initCert(algorithm, bits)
