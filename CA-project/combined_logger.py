5e1678b8c8de8a9c82ca39bf67cf3710c0efb37b
Pi <pchandra@uccs.edu>
2021-03-05 08:12:16 +0000

    enable rainbow

--- CA-project/combined_logger.py
+++ CA-project/combined_logger.py
@@ -48,9 +48,9 @@ def certVerify(algorithm, num_samples, bits = ''):
 
 
 algorithms = [
-	'rsa','dilithium2', 'dilithium3', 'dilithium5', 'falcon512', 'falcon1024', 'dilithium2_aes', 'dilithium3_aes', 'dilithium5_aes', 'rsa3072_dilithium2','rsa3072_falcon512', 'p256_dilithium2', 'p256_falcon512','p384_dilithium3', 'p521_dilithium5', 'p521_falcon1024'] 
+	'rsa','dilithium2', 'dilithium3', 'dilithium5', 'falcon512', 'falcon1024', 'rainbowIclassic','rainbowIcircumzenithal','rainbowIcompressed','rainbowIIIclassic','rainbowIIIcircumzenithal','rainbowIIIcompressed','dilithium2_aes', 'dilithium3_aes', 'dilithium5_aes', 'rsa3072_dilithium2','rsa3072_falcon512', 'p256_dilithium2', 'p256_falcon512','p384_dilithium3', 'p521_dilithium5', 'p521_falcon1024'] 
 algorithms_in_english = [
-	'RSA', 'Dilithium 2', 'Dilithium 3', 'Dilithium 5', 'Falcon 512', 'Falcon 1024', 'Dilithium 2 + AES', 'Dilithium 3 + AES', 'Dilithium 5 + AES', 'RSA 3072 + Dilithium 2','RSA 3072 + Falcon 512', 'P256 + Dilithium 2', 'P256 + Falcon 512','P384 + Dilithium 3', 'P521 + Dilithium 5', 'P521 + Falcon 1024'] 
+	'RSA', 'Dilithium 2', 'Dilithium 3', 'Dilithium 5', 'Falcon 512', 'Falcon 1024', 'Rainbow I Classic','Rainbow I Circumzenithal','Rainbow I Compressed','Rainbow III Classic','Rainbow III Circumzenithal','Rainbow III Compressed', 'Dilithium 2 + AES', 'Dilithium 3 + AES', 'Dilithium 5 + AES', 'RSA 3072 + Dilithium 2','RSA 3072 + Falcon 512', 'P256 + Dilithium 2', 'P256 + Falcon 512','P384 + Dilithium 3', 'P521 + Dilithium 5', 'P521 + Falcon 1024'] 
 
 def header():
 	line = 'Timestamp,'
@@ -148,7 +148,7 @@ def run(file):
 
 
 
-fileName = 'NEW_LOGGED_OPENSSL.csv'
+fileName = 'NEW_LOGGED_OPENSSL2.csv'
 file = open(fileName, 'w')
 file.write(header() + '\n')
 run(file)
---
+++ CA-project/rainbow/create.py
@@ -0,0 +1,16 @@
+import os 
+
+openssl_dir = os.path.expanduser('~/openssl')
+
+
+myCmd = f'{openssl_dir}/apps/openssl req -x509 -new -newkey rainbowIIIcompressed -keyout key_CA.key -out key_CA.pem -nodes -subj "/CN=oqstest CA" -days 365 -config {openssl_dir}/apps/openssl.cnf'
+os.system(myCmd)
+
+myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithm rainbowIIIcompressed -out key_srv.key'
+os.system(myCmd)
+
+myCmd = f'{openssl_dir}/apps/openssl req -new -key key_srv.key -out key_srv.csr -nodes -subj \'/CN=oqstest server\' -config {openssl_dir}/apps/openssl.cnf'
+os.system(myCmd)
+
+myCmd = f'{openssl_dir}/apps/openssl x509 -req -in key_srv.csr -out key_crt.pem -CA key_CA.pem -CAkey key_CA.key -CAcreateserial -days 365' 
+os.system(myCmd)
